dfdd7251957549ac5a55eca2e356a02b30d1b42e
Your Name <roquevalder@gmail.com>
2025-12-25 14:11:35 +0000

    File changes

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  "projectId": "studio-7853618364-90d09",
+  "projectId": "gestaodefrequencia",
   "appId": "1:648424463640:web:a1719abea055592c35126d",
   "apiKey": "AIzaSyCn7Amvw9sN5HrGwNLJNkLSAxn7GcV5Ey8",
-  "authDomain": "studio-7853618364-90d09.firebaseapp.com",
+  "authDomain": "gestaodefrequencia.firebaseapp.com",
   "measurementId": "",
   "messagingSenderId": "648424463640"
 };
